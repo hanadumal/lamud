d7924262e6fb58f629be8a0f04d6afcf8ce1ee7a
hanadumal <47523442+hanadumal@users.noreply.github.com>
2021-06-28 18:05:04 +0800

    update.

--- src/lamuda/messager/telegram.py
+++ src/lamuda/messager/telegram.py
@@ -1,13 +1,10 @@
 import telegram
 
 
-def send(buffer, sid=1254512302):
+def send(buffer, sid, token):
     """
-    群ID： -545209393
-    私聊ID：1254512302
-    https://api.telegram.org/bot1646500683:AAF4Malw1jx0HjvhWQSQl7uJhdMJ3jVP1VE/getUpdates
     :param buffer:
     :return:
     """
-    bot = telegram.Bot(token="1646500683:AAF4Malw1jx0HjvhWQSQl7uJhdMJ3jVP1VE")
+    bot = telegram.Bot(token=token)
     bot.send_message(chat_id=str(sid), text=buffer)
